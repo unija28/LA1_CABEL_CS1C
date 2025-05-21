a6b93a10f2c88cd4114cefd7a65121f96143ba9e
unija28 <cabellunesa@gmail.com>
2025-05-21 17:56:12 +0800

    Create activity1.js

---
+++ activity1.js
@@ -0,0 +1,9 @@
+let name = "Lunesa";
+let YouWannaBecomeInYourLife = "programmer";
+let gender = "Female";
+let twitterHandle = "cabellunesa@gmail.com";
+
+console.log (name);
+console.log (YouWannaBecomeInYourLife);
+console.log (gender);
+console.log (twitterHandle);
